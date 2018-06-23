8f254b560d1471f2c27e158fd92c3b12cf25e111
Alexander Spence <alexander@diversityarrays.com>
2018-06-23 15:40:28 +1000

    Added more error checking for parameters and file existence

--- BSL-CSVReader-CLI/src/main/java/com/spelexander/bsl/BslCsvReader.java
+++ BSL-CSVReader-CLI/src/main/java/com/spelexander/bsl/BslCsvReader.java
@@ -13,23 +13,23 @@ import com.beust.jcommander.ParameterException;
 public class BslCsvReader {
 
 	private static final String PARAMETER_ERROR = "Command Arguments entered were not valid or malformed. See Usage:";
-	
+
 	private static final String PARSING_ERROR = "One or more input file provided had errors. See Errors:";
-	
+
 	private static final String FILE_ERROR = "One or more input files could not be used. See Errors:";
-	
+
 	private static final String EMAIL = "support@fakemail.com";
 	private static final String UNCAUGHT_ERROR = "An unexplained error has occured please contact support at " + EMAIL;
-	
+
 	@Parameter
 	private List<String> parameters = new ArrayList<>();
 
 	@Parameter(names = { "-h", "-help" }, description = "Show usage/help information for tool")
 	private boolean help = false;
-	
+
 	@Parameter(names = { "-log", "-verbose" }, description = "Level of verbosity")
 	private Integer verbose = 1;
-	
+
 	@Parameter(names = { "-p", "-progress" }, description = "Show progress of reading")
 	private Integer progress = 1;
 
@@ -41,7 +41,7 @@ public class BslCsvReader {
 
 	@Parameter(names = "-file", converter = FileConverter.class, description = "Input csv file containing entries to be sorted")
 	File file;
-	
+
 	@Parameter(names = "-output", converter = FileConverter.class, description = "Destination file of sorted entries (defaults to printf)")
 	File output;
 
@@ -54,14 +54,17 @@ public class BslCsvReader {
 			return new File(value);
 		}
 	}
-	
+
+	private static JCommander mainInstance;
+
 	public static void main(String ... argv) {
 		try {
 			BslCsvReader main = new BslCsvReader();
-			JCommander.newBuilder()
-			.addObject(main)
-			.build()
-			.parse(argv);
+			mainInstance =  JCommander.newBuilder()
+					.addObject(main)
+					.build();
+
+			mainInstance.parse();//argv);
 			main.run();
 		} catch (Exception e) {
 			// If we ever get here an actual error has been found in the code.
@@ -76,27 +79,30 @@ public class BslCsvReader {
 	public void run() {
 		// Sorting code runs here
 		try {
-			
-			
-			
-			
+
+			if (this.help) {
+				mainInstance.usage();
+			}
+
 		} catch (Exception e) {
 			if (this.verbose > 1) {
 				e.printStackTrace();
 			}
-			
+
 			// We want to catch all exceptions gracefully
 			if (e instanceof ParameterException) {
 				// Parameter error.
-				
+
 			} else if (e instanceof FileReadException) {
 				// Parsing Error.
-				
+
 			} else if (e instanceof IOException) {
 				// File finding/reading/writing error.
-				
+
 			}
+
+			mainInstance.usage();
 		}
 	}
-	
+
 }
