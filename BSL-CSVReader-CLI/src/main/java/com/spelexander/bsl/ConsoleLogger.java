21a90d0dd93d76a9026358877d0ad004b3c32448
Alexander Spence <alexander@diversityarrays.com>
2018-06-23 15:25:09 +1000

    Logger class - TODO - Later to use console text colouring

---
+++ BSL-CSVReader-CLI/src/main/java/com/spelexander/bsl/ConsoleLogger.java
@@ -0,0 +1,23 @@
+package com.spelexander.bsl;
+
+public class ConsoleLogger {
+
+	// TODO - Add colours and key word.
+	
+	public static void error(String message) {
+		System.out.println(message);	
+	}
+
+	public static void warn(String message) {
+		System.out.println(message);
+	}
+
+	public static void info(String message) {
+		System.out.println(message);
+	}
+
+	public static void print(String message) {
+		System.out.println(message);
+	}
+	
+}
